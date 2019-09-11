7a7f8bf67fe1168eba036b5408ceecb7a125bcba
stupid999 <aierzhiguang2@gmail.com>
2019-09-11 14:23:33 +0800

    新增分數系統，子彈系統，重生系統

--- Assets/CreateGameObj.cs
+++ Assets/CreateGameObj.cs
@@ -8,29 +8,43 @@ public class CreateGameObj : MonoBehaviour
     GameObject good;
 
     [SerializeField]
-    GameObject bad;
+    GameObject badTop;
+
+    [SerializeField]
+    GameObject badMiddle;
+
+    [SerializeField]
+    GameObject badBottom;
 
     [SerializeField]
     GameObject smoke;
 
     Transform self;
 
+    // 重生（用協程，或者invokeRepeating）
+    // 解決重疊（劃分格子，用陣列）
+    // 地板黃色的消失（看youTube）
+    // 調整位置按鈕（那兩個script就enable）
+
+
     // Start is called before the first frame update
     void Start()
     {
         self = GetComponent<Transform>();
-        CreateObj(1, bad);
-        CreateObj(1, good);
-        CreateObj(1, smoke);
+        CreateObj(1, 0.25f, badTop);
+        CreateObj(1, 0.125f, badMiddle);
+        CreateObj(1, 0.05f, badBottom);
+        CreateObj(1, 0.05f, good);
+        CreateObj(1, 0.05f, smoke);
     }
 
-    void CreateObj(int Time,GameObject Obj)
+    void CreateObj(int Time,float PosY,GameObject Obj)
     {
         for (int i = 0; i < Time; i++)
         {
             float randX = Random.Range(-0.45f, 0.45f);
             float randZ = Random.Range(-0.45f, 0.45f);
-            Vector3 ObjPos = new Vector3(randX, 0.05f, randZ);
+            Vector3 ObjPos = new Vector3(randX, PosY, randZ);
             Instantiate(Obj, transform.position + ObjPos, transform.rotation, self);
         }
     }
--- Assets/RayCastTag.cs
+++ Assets/RayCastTag.cs
@@ -2,17 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.UI;
 
 public class RayCastTag : MonoBehaviour
 {
     Vector2 touchPosition;
+    Camera arCamare;
+    Text bulletNumText;
+    Text badScoreText;
+    Text goodScoreText;
 
     [SerializeField]
-    Camera arCamare;
+    GameObject bad;
 
-    private void Update()
+    [SerializeField]
+    GameObject smoke;
+
+    int bulletNum;
+    int badScore;
+    int goodScore;
+
+    Transform self;
+
+    void Start()
     {
-        if (Input.touchCount > 0)
+        self = GetComponent<Transform>();
+        bulletNumText = GameObject.Find("BulletNumText").GetComponent<Text>();
+        badScoreText = GameObject.Find("BadScoreText").GetComponent<Text>();
+        goodScoreText = GameObject.Find("GoodScoreText").GetComponent<Text>();
+        arCamare = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        bulletNumText.text = "子彈數量 : " + bulletNum;
+        badScoreText.text = "被射器官數 : " + badScore;
+        goodScoreText.text = "被射垃圾桶數 : " + goodScore;
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0) 
         {
             Touch touch = Input.GetTouch(0);
 
@@ -25,25 +51,47 @@ public class RayCastTag : MonoBehaviour
 
                 if (Physics.Raycast(ray, out hitObj))
                 {
-                    if(hitObj.transform.tag=="Good")
-                    {
-                        Destroy(hitObj.transform.gameObject);
-                    }
-                    if (hitObj.transform.tag == "Bad")
-                    {
-                        Destroy(hitObj.transform.gameObject);
-                    }
                     if (hitObj.transform.tag == "Smoke")
                     {
                         Destroy(hitObj.transform.gameObject);
+                        bulletNum += 8;
+                        bulletNumText.text = "子彈數量new : " + bulletNum;
+                        Invoke("CreateSmoke", 0.5f);
+                    }
+                    if (hitObj.transform.tag=="Good" && bulletNum > 0)
+                    {
+                        goodScore++;
+                        bulletNum--;
+                        goodScoreText.text = "被射垃圾桶數new : " + goodScore;
+                        bulletNumText.text = "子彈數量new : " + bulletNum;
+                    }
+                    if (hitObj.transform.tag == "Bad" && bulletNum > 0)
+                    {
+                        Destroy(hitObj.transform.gameObject);
+                        bulletNum--;
+                        badScore++;
+                        badScoreText.text = "被射器官數new : " + badScore;
+                        bulletNumText.text = "子彈數量new : " + bulletNum;
+                        Invoke("CreateBad", 0.5f);
                     }
                 }
-
-
-
             }
-
         }
     }
 
+    void CreateBad()
+    {
+        float randX = Random.Range(-0.45f, 0.45f);
+        float randZ = Random.Range(-0.45f, 0.45f);
+        Vector3 ObjPos = new Vector3(randX, 0.05f, randZ);
+        Instantiate(bad, transform.position + ObjPos, transform.rotation, self);
+    }
+
+    void CreateSmoke()
+    {
+        float randX = Random.Range(-0.45f, 0.45f);
+        float randZ = Random.Range(-0.45f, 0.45f);
+        Vector3 ObjPos = new Vector3(randX, 0.05f, randZ);
+        Instantiate(smoke, transform.position + ObjPos, transform.rotation, self);
+    }
 }
