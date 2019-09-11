395228d2cae699b7e5e578550d90036e32260c47
stupid999 <aierzhiguang2@gmail.com>
2019-09-11 09:58:59 +0800

    可以侦测平面 点击平面会产生plant plant上会产生3个方块 点击方块能让方块消失

---
+++ Assets/CreateGameObj.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateGameObj : MonoBehaviour
+{
+    [SerializeField]
+    GameObject good;
+
+    [SerializeField]
+    GameObject bad;
+
+    [SerializeField]
+    GameObject smoke;
+
+    Transform self;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        self = GetComponent<Transform>();
+        CreateObj(1, bad);
+        CreateObj(1, good);
+        CreateObj(1, smoke);
+    }
+
+    void CreateObj(int Time,GameObject Obj)
+    {
+        for (int i = 0; i < Time; i++)
+        {
+            float randX = Random.Range(-0.45f, 0.45f);
+            float randZ = Random.Range(-0.45f, 0.45f);
+            Vector3 ObjPos = new Vector3(randX, 0.05f, randZ);
+            Instantiate(Obj, transform.position + ObjPos, transform.rotation, self);
+        }
+    }
+}
---
+++ Assets/RayCastTag.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class RayCastTag : MonoBehaviour
+{
+    Vector2 touchPosition;
+
+    [SerializeField]
+    Camera arCamare;
+
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            touchPosition = touch.position;
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                Ray ray = arCamare.ScreenPointToRay(touch.position);
+                RaycastHit hitObj;
+
+                if (Physics.Raycast(ray, out hitObj))
+                {
+                    if(hitObj.transform.tag=="Good")
+                    {
+                        Destroy(hitObj.transform.gameObject);
+                    }
+                    if (hitObj.transform.tag == "Bad")
+                    {
+                        Destroy(hitObj.transform.gameObject);
+                    }
+                    if (hitObj.transform.tag == "Smoke")
+                    {
+                        Destroy(hitObj.transform.gameObject);
+                    }
+                }
+
+
+
+            }
+
+        }
+    }
+
+}
